9333ca9eb08bf0771f91c11ba4971cda1d8b3a0e
Alberto <albertoherreravargas@gmail.com>
2022-08-31 19:11:35 +0200

    Adding basic layer for components pattern

--- src/routes/02-components-patterns/pages/ShoppingPage.tsx
+++ src/routes/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,20 @@
-import { ProductCard } from "../components/ProductCard"
+import { ProductCard } from "../components/ProductCard";
+
+const product = {
+	id: "1",
+	title: "Coffe Mug - Card",
+	img: "./coffee-mug.png",
+};
 
 export const ShoppingPage = () => {
-    return (
-        <div>
-            <h1>Shopping Store</h1>
-            <hr />
+	return (
+		<div>
+			<h1>Shopping Store</h1>
+			<hr />
 
-            <ProductCard/>
-        </div>
-    )
-}
+			<div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+				<ProductCard product={product} />
+			</div>
+		</div>
+	);
+};
