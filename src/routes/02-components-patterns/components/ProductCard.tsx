1c8c02848150ceef5a525c5918af76048d45f537
Alberto <albertoherreravargas@gmail.com>
2022-08-31 19:21:42 +0200

    Adding first steps for the compound components

--- src/routes/02-components-patterns/components/ProductCard.tsx
+++ src/routes/02-components-patterns/components/ProductCard.tsx
@@ -13,23 +13,41 @@ interface Product {
 	img?: string;
 }
 
+interface ProductButtonsProps {
+	counter: number;
+	increaseBy: (number: number) => void;
+}
+
+export const ProductImage = ({ img = "" }) => {
+	return <img className={styles.productImg} src={img ?? noImage} alt="Product" />;
+};
+
+export const ProductTitle = ({ title }: { title: string }) => {
+	return <span className={styles.productDescription}>{title}</span>;
+};
+
+export const ProductButtons = ({ counter, increaseBy }: ProductButtonsProps) => {
+	return (
+		<div className={styles.buttonsContainer}>
+			<button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+				-
+			</button>
+			<div className={styles.countLabel}> {counter} </div>
+			<button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
+				+
+			</button>
+		</div>
+	);
+};
+
 export const ProductCard = ({ product }: Props) => {
 	const { counter, increaseBy } = useProduct();
 	const { title, img } = product;
 	return (
 		<div className={styles.productCard}>
-			<img className={styles.productImg} src={img ?? noImage} alt="Coffe mug" />
-
-			<span className={styles.productDescription}>{title}</span>
-			<div className={styles.buttonsContainer}>
-				<button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
-					-
-				</button>
-				<div className={styles.countLabel}> {counter} </div>
-				<button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
-					+
-				</button>
-			</div>
+			<ProductImage img={img} />
+			<ProductTitle title={title} />
+			<ProductButtons counter={counter} increaseBy={increaseBy} />
 		</div>
 	);
 };
